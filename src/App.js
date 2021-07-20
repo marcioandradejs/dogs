5d034d7722451aa495c5de6b3193dfea8a4b52c0
Marcio Andrade <marcioandrade.dev@gmail.com>
2021-07-19 23:57:27 -0300

    API e End Points

--- src/App.js
+++ src/App.js
@@ -1,7 +1,13 @@
 import React from 'react';
+import Api from './Api/Api';
+import './App.css';
 
 function App() {
-  return <div>Dogs React</div>;
+  return (
+    <div>
+      <Api />
+    </div>
+  );
 }
 
 export default App;
