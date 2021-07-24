e9ad850c54d977446a547fc6849cf2cf121e7183
Marcio Andrade <marcioandrade.dev@gmail.com>
2021-07-24 14:08:51 -0300

    Lazy e Suspense adicionado a pagina de estatisticas

--- src/Components/User/UserStats.js
+++ src/Components/User/UserStats.js
@@ -4,7 +4,7 @@ import useFetch from '../../Hooks/useFetch';
 import Head from '../Helper/Head';
 import Loading from '../Helper/Loading';
 import Error from '../Helper/Error';
-import UserStatsGraphs from './UserStatsGraphs';
+const UserStatsGraphs = React.lazy(() => import('./UserStatsGraphs'));
 
 const UserStats = () => {
   const { data, error, loading, request } = useFetch();
@@ -21,10 +21,10 @@ const UserStats = () => {
   if (error) return <Error error={error} />;
   if (data)
     return (
-      <div>
+      <React.Suspense fallback={<div></div>}>
         <Head title="Estatísticas" description="Estatísticas do perfil" />
         <UserStatsGraphs data={data} />
-      </div>
+      </React.Suspense>
     );
   else return null;
 };
